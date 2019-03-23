a2229775f8502928b5676626fc87bb9a362f8433
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-03-23 22:19:34 +0530

    fixed

--- eureka-service-registry/src/main/java/com/enrico/microservices/eureka/EurekaApplication.java
+++ eureka-service-registry/src/main/java/com/enrico/microservices/eureka/EurekaApplication.java
@@ -1,17 +1,17 @@
-package com.example.eurekaserviceregistry;
+package com.enrico.microservices.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 //@EnableDiscoveryClient
-public class EurekaServiceRegistryApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
-
 }

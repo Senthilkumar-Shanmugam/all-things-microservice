d0d68061abfda9f658a3fb21dcbc3da57b037b05
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-03-23 22:33:17 +0530

    fixed

--- zuul-gateway/src/main/java/com/enrico/microservices/zuul/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/enrico/microservices/zuul/ZuulGatewayApplication.java
@@ -1,15 +1,14 @@
-package com.example.zuulgateway;
+package com.enrico.microservices.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@SpringBootApplication
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
-
 }

dcf1a945a4bbfeba46ee20daaa05d384a06917a0
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-03-23 19:56:55 +0530

    service registry

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -2,8 +2,11 @@ package com.example.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEur
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {

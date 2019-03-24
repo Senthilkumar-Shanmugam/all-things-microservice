0a67b1469072f4849a998a70b5e284c75a9cecf2
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-03-24 10:24:31 +0530

    gateway configs added

--- zuul-gateway/src/main/java/com/enrico/microservices/zuul/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/enrico/microservices/zuul/ZuulGatewayApplication.java
@@ -2,10 +2,12 @@ package com.enrico.microservices.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
